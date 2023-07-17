017760d8a7826f468d7b23aaaeff31f1d21894a7
Wendows7 <asswendo@gmail.com>
2023-07-17 23:59:17 +0700

    update footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -60,6 +60,26 @@
 </div>
 @endforeach
 
+<!-- Footer-->
+<footer class="footer py-4">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-4 text-lg-start">Copyright &copy; Recommendation System {{ now()->format('Y') }}</div>
+            <div class="col-lg-4 my-3 my-lg-0">
+                <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
+            </div>
+            <div class="col-lg-4 text-lg-end">
+                <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
+                <a class="link-dark text-decoration-none" href="#!">Terms of Use</a>
+            </div>
+           </div>
+    </div>
+</footer>
+<!-- Portfolio Modals-->
+<!-- Portfolio item 1 modal popup-->
+
 
 
         <!-- Bootstrap core JS-->
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -35,18 +35,5 @@
         {{ $products->links() }}
     </div>
 </section>
-<!-- Footer-->
-<footer class="footer py-4">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-lg-4 text-lg-start">Copyright &copy; {{ now()->format('Y') }}</div>
-            <div class="col-lg-8 text-lg-end">
-                <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
-                <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
-                <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="LinkedIn"><i class="fab fa-instagram"></i></a>
-            </div>
-           </div>
-    </div>
-</footer>
 
 @endsection
